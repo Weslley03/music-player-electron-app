ebb2cbb84c7f2296ac2f19a4cf07db1c422d8006
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-07-17 22:42:30 -0300

    fix: adjusts GenericLayout

---
+++ src/ui/components/MediaPlayer/MediaPlayer.tsx
@@ -0,0 +1,7 @@
+// import styles from './MediaPlayer.module.scss';
+
+const MediaPlayer = () => {
+  return (<></>);
+}
+
+export default MediaPlayer;
--- src/ui/layouts/GenericLayout.tsx
+++ src/ui/layouts/GenericLayout.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import HeaderGlobal from '../components/HeaderGlobal/HeaderGlobal';
 import MyLibrary from '../components/MyLibrary/MyLibrary';
+import MediaPlayer from '../components/MediaPlayer/MediaPlayer';
+import styles from './GenericLayout.module.scss';
 
 type Props = {
   children: React.ReactNode;
 };
 
-const GenericLayout: React.FC<Props> = ({
-  children,
-}) => {
+const GenericLayout: React.FC<Props> = ({ children }) => {
   return (
     <div>
-      <HeaderGlobal />
-      <MyLibrary />
-      {children}
+      <header className={styles.header}>
+        <HeaderGlobal />
+      </header>
+
+      <div className={styles.mainContent}>
+        <aside className={styles.sidebar}>
+          <MyLibrary />
+        </aside>
+
+        <main className={styles.routesArea}>
+          {children}
+        </main>
+      </div>
+
+      <footer className={styles.mediaPlayer}>
+        <MediaPlayer />
+      </footer>
     </div>
   );
 };
