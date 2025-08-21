8a59e196b0c4f14226699afce07d032ac1263f62
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-08-21 19:06:33 -0300

    feat: add Row component in HomePage

--- src/ui/pages/Home/HomePage.tsx
+++ src/ui/pages/Home/HomePage.tsx
@@ -1,10 +1,12 @@
+import Row from '../../components/Row/Row';
 import styles from './HomePage.module.scss';
 
 const HomePage = () => {
   return (
     <div className={styles.mainContainer}>
-      <p> HomePage! </p>
-      <div>
+      <div className={styles.content}>
+        <Row title='musicas melhores q a sua escolha' />
+        <Row title='os maiores sucessos' />
       </div>
     </div>
   )
