001951829ef3c5d0df057b2468c7a187b5b0a7e8
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-08-23 23:40:30 -0300

    fix: apply Skeleton use in MyLibrary and HomePage

--- src/ui/components/Row/Row.tsx
+++ src/ui/components/Row/Row.tsx
@@ -1,29 +1,34 @@
 import type React from "react";
 import styles from './Row.module.scss';
+import type { Album } from "../../types/album-type";
+import type { Artist } from "../../types/artist-type";
+import Skeleton from "../Skeleton/Skeleton";
+
+type CardItem = Album | Artist;
 
 type RowProps = {
   title: string
+  cards: CardItem[]
 };
 
 const Row: React.FC<RowProps> = ({
   title,
+  cards,
 }) => {
-  const mock = [
-    { src: 'https://images.uncyclomedia.co/necyklopedie/cs/c/cc/Taylor-swift.jpg' },
-    { src: 'https://images.uncyclomedia.co/necyklopedie/cs/c/cc/Taylor-swift.jpg' },
-    { src: 'https://images.uncyclomedia.co/necyklopedie/cs/c/cc/Taylor-swift.jpg' },
-    { src: 'https://images.uncyclomedia.co/necyklopedie/cs/c/cc/Taylor-swift.jpg' },
-  ];
 
   return (
     <div className={styles.content}>
       <span className={styles.title}> {title} </span>
       <div className={styles.options}>
-        {mock && mock.length > 0 && mock.map((item) => (
+        {cards && cards.length > 0 ? cards.map((item) => (
           <div className={styles.option}>
-            <img className={styles.artistCapa} src={item.src} />
+            <img className={styles.artistCapa} src={`data:image/jpeg;base64,${item.img}`} />
           </div>
-        ))}
+        ))
+          : (
+            <Skeleton width="180px" height="150px" borderRadius="8px" />
+          )
+        }
       </div>
     </div>
   );
